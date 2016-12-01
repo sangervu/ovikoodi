90287107e888b02625ffb9ee5f5df566e9f8620f
Sakari Angervuori <sakari.angervuori@kotiposti.net>
2016-12-01 18:00:07 +0200

    Add files via upload

--- www/js/ovikoodi.js
+++ www/js/ovikoodi.js
@@ -3,7 +3,19 @@
  */
 function sendInfo(){
 	
+	var monthNumbers = ["1830", "7620", "4513", "7913", "6789", "3241",
+	             		"4273", "3072", "0632", "8156", "5963", "9670"
+	            		];
+	var date = new Date();
 	
+	var popUpText;
 	
+	popUpText = "Pirkko ja Sakari" + "<br>" 
+		+"Rantaharju 8 A 14, Espoo" + "<br>" 
+		+"Ovikoodi = " +monthNumbers[date.getMonth()].bold().fontcolor("red") + "<br>" +
+		"Tervetuloa!";
 	
+	document.getElementById("popup").innerHTML = popUpText;
+	/*document.getElementById("ovikoodi").innerHTML = monthNumbers[date.getMonth()];
+	*/
 }
